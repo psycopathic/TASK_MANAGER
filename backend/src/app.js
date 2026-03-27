9c9f5d9ebf1234e900ebd2db011a3eb31408ffad
psycopathic <harshsrivastava2502@gmail.com>
2026-03-27 17:49:18 +0530

    render

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import apiRoutes from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const allowedOrigins = (process.env.CORS_ORIGIN || "")
 	.split(",")
@@ -25,6 +29,21 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", apiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	const distPath = path.join(__dirname, "../../frontend/dist");
+	app.use(express.static(distPath));
+
+	// Return React app for non-API routes.
+	app.get(/^(?!\/api).*/, (req, res) => {
+		res.sendFile(path.join(distPath, "index.html"));
+	});
+} else {
+	app.get("/", (req, res) => {
+		res.send("Hello World!");
+	});
+}
+
 app.use(errorHandler);
 
 export default app;
